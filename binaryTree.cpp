e0302abf8ea2e4ea899b50288c726a21ace002af
Faiqfarmz <faiqfarmz@gmail.com>
2025-06-26 11:24:40 +0700

    step 2

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -30,7 +30,10 @@ class BinaryTree{
               // Step 1: Allocate memory for the new node
             Node *newNode = new Node();
 
+            // Step 2: Assign value to the data field of new node
+            newNode->info = x;
 
+ 
 
             
     }
