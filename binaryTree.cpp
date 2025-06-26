3b6c7342cfcc7e423855aaf17f7e66c331014e34
Faiqfarmz <faiqfarmz@gmail.com>
2025-06-26 11:29:25 +0700

    step 6

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -50,6 +50,17 @@ class BinaryTree{
 
                 // 5b: Exit
                 return;
+
+            }
+
+            // Step 6: If the value in the data field of new node is less than that
+            if (x < parent->info)
+            {
+                // 6a: Make the left child of parent point to the new node
+                parent->leftchild = newNode;
+
+                // 6b exit
+                return;
             }
 
             
